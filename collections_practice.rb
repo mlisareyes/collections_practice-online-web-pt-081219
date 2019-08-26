06da19072217a6f98c3334d69020e6ee722735dc
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-26 19:27:57 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array. sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array [2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |character|
+    character[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, n|
+    sum + n
+  end
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index == 1
+      element
+    else
+      element + "s"
+    end
+  end
+end
